37e08454248533f397299ab858cb82b4800251a2
adryannegorden <adryannegorden@gmail.com>
2023-10-30 16:03:24 -0400

    changed colors to be more appealing to the eye

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-// "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean" <- medium difficulty game trivia 10 questions
-//fetch("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean")
-    //.then(res => res.json())
-    //.then(data => console.log(data))
-
 // "http://shibe.online/api/shibes?count=10&urls=false&httpsUrls=false" <- more questions u get right more shibas you get POG
 //fetch("http://shibe.online/api/shibes?count=10&urls=false&httpsUrls=false")
     //.then(res => res.json())
     //then(data => console.log(data))
 
-fetch("https://api.adviceslip.com/advice")
-    .then((resp) => resp.json())
-    .then((json) => console.log(json))
-    
+//fetch("https://api.adviceslip.com/advice")
+    //.then((resp) => resp.json())
+    //.then((json) => console.log(json))
+
+// "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean" <- medium difficulty game trivia 10 questions
+//fetch("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean")
+    //.then(res => res.json())
+    //.then(data => console.log(data))
+
+function quizQuestion(){
+    const question = fetch('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=boolean')
+    .then (res => res.json())
+    .then (data => console.log(data))
+}
