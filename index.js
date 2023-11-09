f8e633592aede9eb9292b2e956a912ff4102fa69
adryannegorden <adryannegorden@gmail.com>
2023-11-09 17:12:05 -0500

    adding feature to select what difficulty question you want, also made all fetch requests into functions that can be called rather than having everything load at once

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
+//== API FETCH FUNCTIONS ==\\
     //quiz question api
 let quizAnswer;
 let quizQuestion;
 let decodedQuestion;
-fetch("https://opentdb.com/api.php?amount=10&category=15&type=boolean&encode=base64")
-    .then(res => res.json())
-    .then(data => {
-        quizQuestion = data;
-        decodedQuestion = atob(quizQuestion.results[0].question)
-        document.getElementById('question').textContent = decodedQuestion
-        quizAnswer = quizQuestion.results[0].correct_answer
-    });
+function generateQuizQuestion() {
+        fetch("https://opentdb.com/api.php?amount=50&category=15&type=boolean&encode=base64")
+            .then(res => res.json())
+            .then(data => {
+                quizQuestion = data;
+                decodedQuestion = atob(quizQuestion.results[0].question)
+                document.getElementById('question').textContent = decodedQuestion
+                quizAnswer = quizQuestion.results[0].correct_answer
+    });    
+};
 
     //shiba picture api
 let shibePicture;
-fetch("https://shibe.online/api/shibes?count=10&urls=true&httpsUrls=true")
-    .then(res => res.json())
-    .then(data => {
-        shibePicture = data;
-        document.getElementById('shibeImage').src = shibePicture[0];
-});
+function generateShibePicture() {
+        fetch("https://shibe.online/api/shibes?count=10&urls=true&httpsUrls=true")
+        .then(res => res.json())
+        .then(data => {
+            shibePicture = data;
+            document.getElementById('shibeImage').src = shibePicture[0];
+    });
+};
+generateShibePicture();
 
     //insporational quote api
 let quote;
-fetch("https://api.adviceslip.com/advice")
-    .then(res => res.json())
-    .then(data => {
-        quote = data;
-        document.getElementById('quoteMessage').textContent = quote.slip.advice
-});
+function generateInspoQuote() {
+        fetch("https://api.adviceslip.com/advice")
+        .then(res => res.json())
+        .then(data => {
+            quote = data;
+            document.getElementById('quoteMessage').textContent = quote.slip.advice
+    });
+};
+generateInspoQuote();
 
-//button information
+//== BUTTON INFORMATION ==\\
+//
+
+//true false buttons
 //NOTE: RmFsc2U= MEANS FALSE ;;; VHJ1ZQ== MEANS TRUE ;;; IN BASE64 ENCRYPTION!!!
-
 let userAnswer = null
 const trueButton = document.querySelector('.trueButton');
 trueButton.addEventListener('click', function() {
@@ -56,4 +67,3 @@ falseButton.addEventListener('click', function() {
         incorrectAnswer.style.display = 'block'
     }
 });
-  
